7cc3d999aedf54864369b7fffab79c6e8311f0de
Mikko Paulo Guevarra <emphigie10@gmail.com>
2020-10-15 18:43:53 +0200

    jsnack7

---
+++ js-jsnack7/main.js
@@ -0,0 +1,10 @@
+// Calcola la somma e la media dei numeri da 1 a 10.
+totSum = 0
+for (var i = 0; i < 10; i++) {
+    totSum = i + 1 + totSum;
+}
+console.log("la somma è " + totSum);
+
+media = totSum / 10;
+
+console.log("la media è " + media);
