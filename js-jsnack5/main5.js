fbc743e1e6d6d08310d29db236b15f89420a6b18
Mikko Paulo Guevarra <emphigie10@gmail.com>
2020-10-15 17:41:30 +0200

    rifaccio jsnack5

--- js-jsnack5/main5.js
+++ js-jsnack5/main5.js
@@ -1,7 +1,9 @@
 //stampa il cubo dei N numeri, dove N è un numero indicato dall'utente
-
+//chiedo il numero
 var userChoice = parseInt(prompt("Inserisci il numero che vuoi che trasformi al cubo"));
-
-var numeroCubo = userChoice * userChoice * userChoice;
-
-console.log("il cubo di " + userChoice + " è " + numeroCubo);
+//inizio ciclo for per fare il cubo di N numeri
+for (var i = 0; i < userChoice.length; i++) {
+    //creo variabile di supporto
+    var currentNumber = userChoice[i];
+    console.log(currentNumber);
+}
---
+++ js-jsnack6/main.js
@@ -0,0 +1,6 @@
+//Stampa le potenze di 2 senza superare il numero 1000.
+
+for (var i = 0; i < 10; i++) {
+
+    
+}
